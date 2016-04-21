634e216474484e93e66aa135c9d70d87cadbfe63
Mandeep <mandeep.singh.bhatia@gmail.com>
2016-04-21 11:01:29 +0800

    timer util begining

---
+++ src/behavior-utils.scm
@@ -0,0 +1,23 @@
+(use-modules (srfi srfi-1) )
+
+;(add-to-load-path "/usr/local/share/opencog/scm")
+;(use-modules (opencog))
+;(use-modules (opencog opencog exec))
+;(use-modules (opencog openpsi))
+
+(define timer-list '())
+;elps is pair of seconds and microseconds
+(define (set-timer id elps) (set! timer-list (append timer-list (list(list id elps (gettimeofday))))))
+
+(define (get-timer-info id) (filter (lambda (x)(= id (car x))) timer-list))
+
+(define (peek-timer id) (let ((tmr (get-timer-info id)))(if (= 1 (length tmr)) 
+							(let* ( (tm (cdr (car tmr))) (elp (car tm)) (st (car (cdr tm))) 
+								(el-sec (car elp)) (el-ms (cdr elp)) (st-sec (car st)) (st-ms (cdr st))
+								(ct (gettimeofday)) (ct-sec (car ct)) (ct-ms (cdr ct))
+								(pt-sec (- ct-sec st-sec)) (pt-ms (- ct-ms st-ms))
+								)
+								(if (> pt-sec el-sec) #t (and (= pt-sec el-sec) (> pt-ms el-ms) ) )
+							)
+							#f))
+)
